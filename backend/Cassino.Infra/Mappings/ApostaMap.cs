9fbc4d31822c9eac568740412f4e57233f2526a2
Yohans Nascimento <yohans.dev@gmail.com>
2023-05-12 18:52:24 -0300

    added ApostaMap

---
+++ backend/Cassino.Infra/Mappings/ApostaMap.cs
@@ -0,0 +1,25 @@
+﻿using Cassino.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cassino.Infra.Mappings
+{
+    public class ApostaMap : IEntityTypeConfiguration<Aposta>
+    {
+        public void Configure(EntityTypeBuilder<Aposta> builder)
+        {
+            builder
+                .Property(a => a.IdUsuario);
+
+            builder
+                .Property(a => a.Valor)
+                .IsRequired()
+                .HasPrecision(18, 2);
+        }
+    }
+}
