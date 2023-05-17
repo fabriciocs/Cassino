7cff0fad2a09eab42f9f98b06ef94c8350e9de78
Yohans Nascimento <yohans.dev@gmail.com>
2023-05-17 15:05:57 -0300

    added AdmApostasController

---
+++ backend/Cassino.Api/Controllers/V1/Gerencia/AdministradorApostaController.cs
@@ -0,0 +1,35 @@
+﻿using Cassino.Application.Contracts;
+using Cassino.Application.Notification;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cassino.Api.Controllers.V1.Gerencia
+{
+    [Route("v{version:apiVersion}/Aposta/[controller]")]
+    public class AdministradorApostaController : BaseController
+    {
+        private readonly IApostaService _apostaService;
+        public AdministradorApostaController(IApostaService apostaService, INotificator notificator) : base(notificator)
+        {
+            _apostaService = apostaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuscarTodasApostas()
+        {
+            var apostas = _apostaService.ObterTodasApostas();
+            if(apostas == null)
+                return NotFound();
+            return Ok(apostas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BuscarApostaUsuario(int id)
+        {
+            var apostasUsuario = _apostaService.ObterApostasDeUsuario(id);
+            if(apostasUsuario == null)
+                return NotFound();
+            return Ok(apostasUsuario);
+        }
+
+    }
+}
