8090bfd3b259bcbf41ae41bc4f394be9f6bfe48e
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2023-06-11 14:34:43 -0300

    Feat: working on withdraw

---
+++ backend/Cassino.Infra/Migrations/20230611084327_AddPagamentos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cassino.Infra.Migrations
+{
+    public partial class AddPagamentos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataPagamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataExpiracaoPagamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Conteudo = table.Column<string>(type: "nvarchar(max)", maxLength: 6000, nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    PagamentoId = table.Column<int>(type: "int", nullable: false),
+                    Aprovado = table.Column<bool>(type: "bit", nullable: false),
+                    Desativado = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    CriadoPor = table.Column<int>(type: "int", nullable: true),
+                    CriadoPorAdmin = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    AtualizadoPor = table.Column<int>(type: "int", nullable: true),
+                    AtualizadoPorAdmin = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pagamentos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamentos_UsuarioId",
+                table: "Pagamentos",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+        }
+    }
+}
