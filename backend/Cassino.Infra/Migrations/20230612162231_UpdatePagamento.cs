30b3d59a692684d61e918e37d740f6ee91ac979b
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2023-06-13 10:45:38 -0300

    Feat: working on withdraw and pix

---
+++ backend/Cassino.Infra/Migrations/20230612162231_UpdatePagamento.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cassino.Infra.Migrations
+{
+    public partial class UpdatePagamento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PagamentoId",
+                table: "Pagamentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PagamentoId",
+                table: "Pagamentos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
