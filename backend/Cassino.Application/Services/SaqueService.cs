0fa4d2ccb5e7bc2c5631e2437956a5d77d7a973d
felipebsoares <103378305+felipebsoares@users.noreply.github.com>
2023-07-06 18:14:34 -0300

    Feat: changed to gerencianet...

--- backend/Cassino.Application/Services/SaqueService.cs
+++ backend/Cassino.Application/Services/SaqueService.cs
@@ -4,6 +4,7 @@ using Cassino.Application.Dtos.V1.Pagamentos;
 using Cassino.Application.Notification;
 using Cassino.Domain.Contracts.Repositories;
 using Cassino.Domain.Entities;
+using Gerencianet.NETCore.SDK;
 
 namespace Cassino.Application.Services;
 
@@ -17,9 +18,18 @@ public class SaqueService : BaseService, ISaqueService
     
     public async Task<Saque?> Adicionar(SaqueDto saque)
     {
-
+        var assemblyPath = Path.GetDirectoryName(typeof(DependencyInjection).Assembly.Location);
+        var pasta = "\\Certificates\\producao-467170-jogosProducao.p12";
+        var path = assemblyPath + pasta;
+        // X509Certificate2 uidCert = new X509Certificate2(path, "");
+        
+        
+        dynamic endpoints = new Endpoints("Client_Id_e53549ce91fe086e73d44d9238d3770e2ad08035", "Client_Secret_1485ed3cda1cd75dde165c8578b1aa8f0c63f2c1", false, path);
+        
+                
         var extract = Mapper.Map<Saque>(saque);
 
+        
         extract.Aprovado = false;
         extract.DataSaque = DateTime.Now;
         _repository.Adicionar(extract);
